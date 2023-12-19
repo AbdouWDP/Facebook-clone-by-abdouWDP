e34eeee7a7fb81fe4e30825a6fb06d8227a244d7
AbdouTheWD <gachtou3@gmail.com>
2023-12-19 03:18:14 +0100

    change login path

--- JS/home.js
+++ JS/home.js
@@ -260,7 +260,7 @@ logout_btn.addEventListener("click", () => {
     localStorage.removeItem("token");
     localStorage.removeItem("cursor_location");
     localStorage.removeItem("user");
-    location.href = "/HTML/login.html";
+    location.href = "/login.html";
   }
 });
 
