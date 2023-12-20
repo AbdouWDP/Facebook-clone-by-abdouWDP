ad9af413a0abe9773e550da9979e8a75d83f7f43
AbdouTheWD <gachtou3@gmail.com>
2023-12-20 11:22:42 +0100

    update roles

--- JS/home.js
+++ JS/home.js
@@ -104,19 +104,19 @@ async function getPosts() {
                 <div class="post-date">${p.created_at}</div>
               </div>
               <div class="post-buttons">
-                <button class="choose-button" type="button">
+                <button class="choose-button" type="button" role="choose-post-properety">
                   <i class="fa-solid fa-ellipsis"></i>
                 </button>
                 <div class="buttons">
                   <button class="update-post" onclick="updatePost(${
                     p.id
-                  })" type="button">
+                  })" type="button" role="update-post-button">
                     <i class="fa-solid fa-arrows-rotate"></i>
                     <span>Update post</span>
                   </button>
                   <button class="delete-post" onclick="deletePost(${
                     p.id
-                  })" type="button">
+                  })" type="button" role="delete-post-button">
                     <i class="fa-solid fa-trash"></i>
                     <span>Delete post</span>
                   </button>
